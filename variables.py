c8f17fbc76069ce8cfe21789f1eef830b88b1a7b
SantiagoHM27 <himejimadarkrai@gmail.com>
2023-06-08 14:33:15 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+# Python es No tipado
+name ="Santiago"
+age = "20"
+is_single = True
+print(name)
+print(is_single)
+
+"""
+
+Hola soy Santiago, tengo 20 años y soltero = True
+
+"""
+#Utilizan format String
+print("Hola soy {}, tengo {} años y soltero = {}".
+format(name, age, is_single) )
